6e18ff4876a078c07271735fbbfada60f1b9deae
Kevin Bravo <kbravo1024@gmail.com>
2023-03-16 18:17:37 -0700

    complete first class function lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+
+
+const returnFirstTwoDrivers = function(array){
+    return array.slice(0,2);
+}
+
+const returnLastTwoDrivers = function(array){
+    return [array[array.length - 2], array[array.length - 1]];
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(interger){
+    return (function(fare){
+        return fare * interger;
+    })
+} 
+
+function fareDoubler(fare){
+    const newFare = createFareMultiplier(2)(fare);
+    return newFare;
+}
+
+function fareTripler(fare){
+    const newFare = createFareMultiplier(3)(fare);
+    return newFare;
+}
+
+function selectDifferentDrivers(array, func){
+    return(func(array));
+}
